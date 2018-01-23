c325ea6f7fcf9bf5b01922d6c6a2d7f3a83c5c8c
Max Damage <maxdamaj@yandex.ru>
2018-01-23 23:52:03 +0300

    Level Window (custom window in editor)

---
+++ Assets/Editor/LevelWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using BugRush.System;
+
+public class LevelWindow : EditorWindow {
+
+    [MenuItem("Window/Level Editor")]
+
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(LevelWindow));
+    }
+
+    public Vector2 scrollView;
+
+    void OnGUI() {
+        Level level = GlobalData.Instance.level;
+        GUILayout.Label(level.title, EditorStyles.boldLabel);
+
+        scrollView = EditorGUILayout.BeginScrollView(scrollView);
+
+        for (int i = 0; i < level.cells.Count; i += level.columns) {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField((i/level.columns).ToString(), EditorStyles.boldLabel, GUILayout.Width(20));
+            for (int j = 0; j < level.cells.Count / level.rows; j++) {
+                EditorGUILayout.IntField(level.cells[i+j], GUILayout.Width(20));
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+}
